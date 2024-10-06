900ac72b532b4cabbb0250f9a011431cbee61fea
thanhren <40245250+thanhren@users.noreply.github.com>
2024-10-06 10:08:19 +0700

    Update done.sh
    Signed-off-by: thanhren <40245250+thanhren@users.noreply.github.com>

--- done.sh
+++ done.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-# Append the content of Task contract/erc20tx.txt to erc20.txt
+# Append the content of Task contract/contracterc20tx.txt to erc20.txt
 cat "Task contract/contracterc20tx.txt" >> "Task contract/contracterc20.txt"
 
-# Append the content of Task contract/erc721.txt to erc20.txt
+# Append the content of Task contract/contracterc721.txt to erc20.txt
 cat "Task contract/contracterc721.txt" >> "Task contract/contracterc20.txt"
 
-# Append the content of Task contract/erc20tx.txt to erc20.txt
+# Append the content of Task contract/contracterc721tx.txt to erc20.txt
 cat "Task contract/contracterc721tx.txt" >> "Task contract/contracterc20.txt"
 
 # Append the content of Task contract/erc721.txt to erc20.txt
 cat "Task contract/contractperc20.txt" >> "Task contract/contracterc20.txt"
 
-# Append the content of Task contract/erc20tx.txt to erc20.txt
+# Append the content of Task contract/contractperc20tx.txt to erc20.txt
 cat "Task contract/contractperc20tx.txt" >> "Task contract/contracterc20.txt"
 
-# Append the content of Task contract/erc721.txt to erc20.txt
+# Append the content of Task contract/contractprivate.txt to erc20.txt
 cat "Task contract/contractprivate.txt" >> "Task contract/contracterc20.txt"
 
 # Append the content of Task contract/erc20tx.txt to erc20.txt
-cat "Task contract/contractproxy.txt" >> "Task contract/contracterc20.txt"
+cat "Task contract/contractproxyaddress1.txt" >> "Task contract/contracterc20.txt"
 
 # Append the content of Task contract/erc721.txt to erc20.txt
 cat "Task contract/contractproxytx.txt" >> "Task contract/contracterc20.txt"
@@ -27,9 +27,15 @@ cat "Task contract/contractproxytx.txt" >> "Task contract/contracterc20.txt"
 # Append the content of Task contract/erc20tx.txt to erc20.txt
 cat "Task contract/contracthardhat.txt" >> "Task contract/contracterc20.txt"
 
+# Append the content of Task contract/erc20tx.txt to erc20.txt
+cat "Task contract/contractproxyaddress2.txt" >> "Task contract/contracterc20.txt"
+
+# Append the content of Task contract/erc20tx.txt to erc20.txt
+cat "Task contract/contractproxy.txt" >> "Task contract/contracterc20.txt"
 
 # Append the content of Task contract/erc20tx.txt to erc20.txt
 cat "Task contract/contracterc20.txt" >> "Task contract/full.txt"
+
 # Append the content of Task contract/erc20tx.txt to erc20.txt
 echo "" >> "Task contract/full.txt"
 
